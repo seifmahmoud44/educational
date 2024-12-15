12ef6918664fb4ec9aa140a3978e8c8f6a64f4ba
seif <seenaseif@gmail.com>
2024-12-15 13:23:16 +0200

    fix nav bar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -17,8 +17,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar } from "@/components/ui/avatar";
